8742acddeca3411ea2e89c79ec2bb73c31e4dfb6
matejkajn <matejka.jn@gmail.com>
2025-11-05 20:04:59 +0100

    init

---
+++ src/components/ProfileCard.tsx
@@ -0,0 +1,33 @@
+type Props = {
+  image: string;
+  name: string;
+  description: string;
+  phone: string;
+  content: string;
+};
+
+const ProfileCard = ({ image, name, description, phone, content }: Props) => {
+  return (
+    <div className="flex items-center gap-4 p-4">
+      <div className="no-flex">
+        <div className="items-center flex">
+          <img
+            className="w-[108px] h-[108px] rounded-full"
+            src={`src/assets/profiles/${image}`}
+            alt=""
+          />
+          <div className="no-flex px-4">
+            <div className="font-bold font-times">{name}</div>
+            {description && <div className="text-sm">{description}</div>}
+            <a className="text-sm" href={`tel:${phone}`}>
+              {phone}
+            </a>
+          </div>
+        </div>
+        <div>{content && <div className="py-3 text-sm">{content}</div>}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileCard;
---
+++ src/components/ProjectCard.tsx
@@ -0,0 +1,21 @@
+type Props = {
+  image: string;
+  name: string;
+  description: string;
+  author: string;
+};
+
+const ProjectCard = ({ image, name, description, author }: Props) => {
+  return (
+    <div className="py-5">
+      <img className="w-full h-auto" src={`src/assets/projects/${image}`} alt="" />
+      <div className="pt-4">
+        <div className="font-bold p-0 m-0">{name}</div>
+        <div className="p-0 m-0">{description}</div>
+        <div className="font-bold p-0 m-0">{author}</div>
+      </div>
+    </div>
+  );
+};
+
+export default ProjectCard;
